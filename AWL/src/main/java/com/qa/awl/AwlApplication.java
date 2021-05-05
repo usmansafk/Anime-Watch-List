2ea1db8d7a01a0660f54c51f610b779a29536589
Usman Sajid <usman230@hotmail.co.uk>
2021-05-05 15:51:33 +0100

    US:init backend package structure commit

---
+++ AWL/src/main/java/com/qa/awl/AwlApplication.java
@@ -0,0 +1,13 @@
+package com.qa.awl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwlApplication.class, args);
+	}
+
+}
---
+++ AWL/src/test/java/com/qa/awl/AwlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qa.awl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
