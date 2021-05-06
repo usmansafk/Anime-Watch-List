b2650d325a224b453dd886e1e87bce8528e76954
Usman Sajid <usman230@hotmail.co.uk>
2021-05-06 19:27:17 +0100

    US:created new repo package/file

---
+++ AWL/src/main/java/com/qa/awl/repo/AWLRepo.java
@@ -0,0 +1,15 @@
+package com.qa.awl.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.qa.awl.domain.AWL;
+
+@Repository
+public interface AWLRepo extends JpaRepository<AWL, Long> {
+	
+	AWL findByName(String name);
+	
+	
+
+}
