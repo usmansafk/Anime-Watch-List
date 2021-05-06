1b9b37320dc50e494d614b48a71c203f8fc53116
Usman Sajid <usman230@hotmail.co.uk>
2021-05-06 19:29:02 +0100

    US:created AWL-Service-DB file to utilzed instead of static list

---
+++ AWL/src/main/java/com/qa/awl/service/AWLServiceDB.java
@@ -0,0 +1,59 @@
+package com.qa.awl.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import com.qa.awl.domain.AWL;
+import com.qa.awl.repo.AWLRepo;
+
+
+@Service
+public class AWLServiceDB implements AWLService{
+	
+	private AWLRepo repo; // <-- dependency
+	
+	public AWLServiceDB(AWLRepo repo) { // <-- injection
+		this.repo = repo;
+	}
+	
+
+	@Override
+	public AWL create(AWL a) {
+		return this.repo.saveAndFlush(a);
+	}
+
+	@Override
+	public List<AWL> getAll() {
+		return this.repo.findAll();
+	}
+
+	@Override
+	public AWL getByID(Long id) {
+		Optional<AWL> optionalAnime = this.repo.findById(id);
+		return optionalAnime.get();
+	}
+	
+	@Override
+	public AWL getAnimeByName(String name) {
+		return this.repo.findByName(name);
+	}
+
+	@Override
+	public AWL update(Long id, AWL newAnime) {
+		AWL anime = this.repo.findById(id).orElseThrow();
+		anime.setName(newAnime.getName());
+		anime.setEpisode(newAnime.getEpisode());
+		anime.setRating(newAnime.getRating());
+		return this.repo.saveAndFlush(anime);
+	}
+	
+	@Override
+	public boolean remove(Long id) {
+		this.repo.deleteById(id);
+		return !this.repo.existsById(id);
+	
+	}
+
+}
