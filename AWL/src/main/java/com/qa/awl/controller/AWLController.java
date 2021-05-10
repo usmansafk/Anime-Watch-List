d8ac6b16e229905e1233a7839d0c35a6a6e90425
Usman Sajid <usman230@hotmail.co.uk>
2021-05-10 19:51:41 +0100

    US:enabled cross orgin

--- AWL/src/main/java/com/qa/awl/controller/AWLController.java
+++ AWL/src/main/java/com/qa/awl/controller/AWLController.java
@@ -6,18 +6,22 @@ import javax.websocket.server.PathParam;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.qa.awl.domain.AWL;
 import com.qa.awl.service.AWLService;
 
 @RestController
+//@RequestMapping("/AWL")
+@CrossOrigin(origins = "*")
 public class AWLController {
 
 	// ----- Instance Variables -----
