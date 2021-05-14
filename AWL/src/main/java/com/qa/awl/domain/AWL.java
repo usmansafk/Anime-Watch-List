5741951d4a26815b89eece804ddd0c861d434e41
Usman Sajid <usman230@hotmail.co.uk>
2021-05-14 12:23:20 +0100

    US:resolving internal 500 error

--- AWL/src/main/java/com/qa/awl/domain/AWL.java
+++ AWL/src/main/java/com/qa/awl/domain/AWL.java
@@ -1,5 +1,7 @@
 package com.qa.awl.domain;
 
+import lombok.Builder;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -7,18 +9,6 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.validation.constraints.NotNull;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-@NoArgsConstructor
-@AllArgsConstructor
-@Getter
-@Setter
-@EqualsAndHashCode
 @Entity
 @Builder
 public class AWL {
@@ -27,9 +17,9 @@ public class AWL {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY) // Auto-increments
 	private Long id;
-
+	
 	@NotNull(message = "The name of the anime must be entered")
-	@Column(unique = true)
+	@Column (unique = true)
 	private String name;
 	private int episode;
 	private int rating;
@@ -46,16 +36,16 @@ public class AWL {
 		this.rating = rating;
 	}
 
-//	public AWL(Long id, String name, int episode, int rating) {
-//		super();
-//		this.id = id;
-//		this.name = name;
-//		this.episode = episode;
-//		this.rating = rating;
-//	}
-//
-//	public AWL() {
-//	}
+	public AWL(Long id, String name, int episode, int rating) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.episode = episode;
+		this.rating = rating;
+	}
+
+	public AWL() {
+	}
 
 	// ----Methods----
 	public void setId(Long id) {
@@ -86,41 +76,41 @@ public class AWL {
 		this.rating = rating;
 	}
 
-//	@Override
-//	public int hashCode() {
-//		final int prime = 31;
-//		int result = 1;
-//		result = prime * result + episode;
-//		result = prime * result + ((id == null) ? 0 : id.hashCode());
-//		result = prime * result + ((name == null) ? 0 : name.hashCode());
-//		result = prime * result + rating;
-//		return result;
-//	}
-//
-//	@Override
-//	public boolean equals(Object obj) {
-//		if (this == obj)
-//			return true;
-//		if (obj == null)
-//			return false;
-//		if (getClass() != obj.getClass())
-//			return false;
-//		AWL other = (AWL) obj;
-//		if (episode != other.episode)
-//			return false;
-//		if (id == null) {
-//			if (other.id != null)
-//				return false;
-//		} else if (!id.equals(other.id))
-//			return false;
-//		if (name == null) {
-//			if (other.name != null)
-//				return false;
-//		} else if (!name.equals(other.name))
-//			return false;
-//		if (rating != other.rating)
-//			return false;
-//		return true;
-//	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + episode;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + rating;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		AWL other = (AWL) obj;
+		if (episode != other.episode)
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (rating != other.rating)
+			return false;
+		return true;
+	}
 
 }
