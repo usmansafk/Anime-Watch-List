1b485f1a2fa78e631a676bb9d6585f1b556fe6b1
Usman Sajid <usman230@hotmail.co.uk>
2021-05-14 22:21:31 +0100

    US: controller refactored

--- AWL/src/main/java/com/qa/awl/controller/AWLController.java
+++ AWL/src/main/java/com/qa/awl/controller/AWLController.java
@@ -4,8 +4,6 @@ import java.util.List;
 
 import javax.websocket.server.PathParam;
 
-import lombok.RequiredArgsConstructor;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,11 +14,12 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.qa.awl.domain.AWL;
 import com.qa.awl.service.AWLService;
+
+import lombok.RequiredArgsConstructor;
 
 @RestController
 //@RequestMapping("/AWL")
